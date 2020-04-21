290b103aa8e28b59ce97c249f191c44cd014288c
Beniamin Bia <biabeniamin@outlook.com>
2020-04-21 21:59:46 +0300

    create game component and creating rounting for it

--- PresidentFront/src/app/app.module.ts
+++ PresidentFront/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { 
+        path : '',
+        component : AppComponent
+      },
+      { 
+        path : 'game',
+        component : GameComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
